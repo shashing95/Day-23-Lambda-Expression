272f7dabc5eaed7668b1364619c43eb878cfbba1
shashi <shashidharngg@gmail.com>
2022-09-05 21:31:18 +0530

    added:Checking users mood

---
+++ MoodAnalyzer.java
@@ -0,0 +1,13 @@
+package com.bridgelabz.lambdaproblems;
+
+public class MoodAnalyzer {
+
+	public static String analyseMood(String mood) {
+		if (mood.toLowerCase().contains("happy")) {
+			return "Entry Successful";
+		} else if (mood.toLowerCase().contains("sad")) {
+			return "Entry Failed";
+		} else return null;
+	}
+}
+
